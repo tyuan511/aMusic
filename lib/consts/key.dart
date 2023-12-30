afec58c42ab89246e12126bdd7d9f850b06b88ba
yuantang <yuantang511@outlook.com>
2023-12-30 21:50:45 +0800

    fix: 🐛 remove other platforms

--- lib/consts/key.dart
+++ lib/consts/key.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 final rootNavigatorKey = GlobalKey<NavigatorState>();
 final shellNavigatorKey = GlobalKey<NavigatorState>();
 
-final configStorageKey = 'CONFIG';
-final playerStorageKey = 'PLAYER';
+const configStorageKey = 'CONFIG';
+const playerStorageKey = 'PLAYER';
